423aa7e7178f837b06bc63d6591d1ab0f1d20cae
Owen Johnson <owen@owenjohnson.info>
2018-11-07 22:15:55 -0600

    Make sure that adding an Item increases the total for the transaction
    Create some test items and a test to check that the value has changed.

--- KataPos/KataPos.Test/TransactionTests.cs
+++ KataPos/KataPos.Test/TransactionTests.cs
@@ -1,16 +1,33 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using KataPos;
+using System.Collections.Generic;
 
 namespace KataPos.Test
 {
     [TestClass]
     public class TransactionTests
     {
+        readonly Dictionary<string, Item> _testCatalog = new Dictionary<string, Item>
+        {
+            ["carrot"] = new Item(),
+            ["dog-food"] = new Item(),
+            ["pear"] = new Item(),
+            ["steak"] = new Item(),
+        };
+
         [TestMethod]
         public void EmptyTransactionHasNoValue()
         {
             var transaction = new Transaction();
             Assert.AreEqual(0, transaction.TotalValue);
+        }
+
+        [TestMethod]
+        public void AddingAndItemAddsValue()
+        {
+            var transaction = new Transaction() { Catalog = _testCatalog };
+            transaction.Scan("dog-food");
+            Assert.AreNotEqual(0, transaction.TotalValue);
         }
     }
 }
--- KataPos/KataPos/Transaction.cs
+++ KataPos/KataPos/Transaction.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace KataPos
 {
     public class Transaction
     {
         public int TotalValue { get; set; }
+        public Dictionary<string, Item> Catalog { get; set; } = new Dictionary<string, Item>();
+
+        public void Scan(string v)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
