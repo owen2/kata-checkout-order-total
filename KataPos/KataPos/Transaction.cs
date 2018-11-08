bed021f5fc715eca89900ef6ec86e071ec435c0e
Owen Johnson <owen@owenjohnson.info>
2018-11-07 22:26:02 -0600

    Add scanned items to transaction
    doh, but the tests still fail...

--- KataPos/KataPos/Transaction.cs
+++ KataPos/KataPos/Transaction.cs
@@ -5,12 +5,17 @@ namespace KataPos
 {
     public class Transaction
     {
+        public IList<Item> Items { get; } = new List<Item>();
+
         public int TotalValue { get; set; }
         public Dictionary<string, Item> Catalog { get; set; } = new Dictionary<string, Item>();
 
-        public void Scan(string v)
+        public void Scan(string barcode)
         {
-            throw new NotImplementedException();
+            if (Catalog.ContainsKey(barcode))
+                Items.Add(Catalog[barcode]);
+            else
+                throw new Exception($"No item matching barcode ${barcode} was found.");
         }
     }
 }
