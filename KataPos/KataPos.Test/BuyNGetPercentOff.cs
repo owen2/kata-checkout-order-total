307169b7535a0ec87dbbbee30d8858d82f22096d
Owen Johnson <owen@owenjohnson.info>
2018-11-09 20:55:36 -0600

    Implement Limits
    and the tests for it

--- KataPos/KataPos.Test/BuyNGetPercentOff.cs
+++ KataPos/KataPos.Test/BuyNGetPercentOff.cs
@@ -5,15 +5,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace KataPos.Test
 {
     [TestClass]
-    public class BuyNGetPercentOffTests 
+    public class BuyNGetPercentOffTests
     {
         [TestMethod]
         public void BuyOneDogfoodGetOneDogfoodFree()
         {
             var order = new Order { Catalog = TestCatalog.Catalog };
             order.Promotions.Add(new BuyNGetPercentOff { Barcode = "dog-food", TriggerQuantity = 1, PercentOff = 1m });
+
             order.Scan("dog-food");
             order.Scan("dog-food");
+
             Assert.AreEqual(20m, order.PreTaxTotal);
         }
 
@@ -22,8 +24,64 @@ namespace KataPos.Test
         {
             var order = new Order { Catalog = TestCatalog.Catalog };
             order.Promotions.Add(new BuyNGetPercentOff { Barcode = "dog-food", TriggerQuantity = 1, PercentOff = 1m });
+
             order.Scan("dog-food");
+
             Assert.AreEqual(20m, order.PreTaxTotal);
+        }
+
+        [TestMethod]
+        public void BuyTwoDogFoodsGetOneDogFoodHalfOff()
+        {
+            var order = new Order { Catalog = TestCatalog.Catalog };
+            order.Promotions.Add(new BuyNGetPercentOff { Barcode = "dog-food", TriggerQuantity = 2, PercentOff = .5m });
+
+            order.Scan("dog-food");
+            order.Scan("dog-food");
+            order.Scan("dog-food");
+
+            Assert.AreEqual(20m + 20m + 10m, order.PreTaxTotal);
+        }
+
+        [TestMethod]
+        public void BuyTwoDogFoodsGetOneDogFoodHalfOffOneExtra()
+        {
+            var order = new Order { Catalog = TestCatalog.Catalog };
+            order.Promotions.Add(new BuyNGetPercentOff { Barcode = "dog-food", TriggerQuantity = 2, PercentOff = .5m });
+
+            order.Scan("dog-food");
+            order.Scan("dog-food");
+            order.Scan("dog-food");
+            order.Scan("dog-food");
+
+            Assert.AreEqual(20m + 20m + 10m + 20m, order.PreTaxTotal);
+        }
+
+        [TestMethod]
+        public void BuyThreeDogFoodsGetOneDogFoodHalfOffDoesntApply()
+        {
+            var order = new Order { Catalog = TestCatalog.Catalog };
+            order.Promotions.Add(new BuyNGetPercentOff { Barcode = "dog-food", TriggerQuantity = 3, PercentOff = .5m });
+
+            order.Scan("dog-food");
+            order.Scan("dog-food");
+            order.Scan("dog-food");
+
+            Assert.AreEqual(60m, order.PreTaxTotal);
+        }
+
+        [TestMethod]
+        public void BuyOneDogFoodGetOneDogFoodFreeLimitOne()
+        {
+            var order = new Order { Catalog = TestCatalog.Catalog };
+            order.Promotions.Add(new BuyNGetPercentOff { Barcode = "dog-food", TriggerQuantity = 1, PercentOff = 1m, Limit = 1 });
+
+            order.Scan("dog-food");
+            order.Scan("dog-food");
+            order.Scan("dog-food");
+            order.Scan("dog-food");
+
+            Assert.AreEqual(60m, order.PreTaxTotal);
         }
     }
 }
--- KataPos/KataPos/BuyNGetPercentOff.cs
+++ KataPos/KataPos/BuyNGetPercentOff.cs
@@ -1,32 +1,33 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace KataPos
 {
-    public class BuyNGetPercentOff :ISpecial
+    public class BuyNGetPercentOff : ISpecial
     {
 
-        public string Barcode
-        {
-            get;
-            set;
-        }
+        public string Barcode { get; set; }
 
-        public int TriggerQuantity
-        {
-            get;
-            set;
-        }
+        public int TriggerQuantity { get; set; }
 
-        public decimal PercentOff
-        {
-            get;
-            set;
-        }
+        public decimal PercentOff { get; set; }
+
+        public int? Limit { get; set; }
 
         public decimal CalculateDiscount(IEnumerable<Item> items)
         {
-            throw new NotImplementedException();
+            var applicableItems = items.OfType<IndividualItem>().Where(item => item.Barcode == Barcode);
+
+            if (!applicableItems.Any())
+                return 0m;
+
+            var price = applicableItems.First().EachesPrice;
+            var discountedItems = applicableItems.Count() / (TriggerQuantity + 1);
+            if (Limit.HasValue)
+                discountedItems = Math.Min(discountedItems, Limit.Value);
+
+            return -discountedItems * price * PercentOff;
         }
     }
 }
